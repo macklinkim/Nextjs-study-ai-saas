c4c164fe7d0cf9b3163fc9e86dc61c4f17871dd4
mackh <kopserf@gmail.com>
2024-05-01 14:37:19 +0900

    🎉 [24.05.01] begin the project

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+type Props = {
+
+}
+const DashboardPage = ({ }: Props) => {
+  return (
+    <div>
+      <Button variant="destructive" size="lg">
+        <p>DashBoard</p>
+      </Button>
+    </div>
+  );
+}
+export default DashboardPage
