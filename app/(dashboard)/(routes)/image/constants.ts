63df997c7ce5cb3eb103c905c25f0e8d48f6729a
mackh <kopserf@gmail.com>
2024-05-03 21:27:29 +0900

    🔨 [24.05.03] image generating만들었다.

--- app/(dashboard)/(routes)/image/constants.ts
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -7,9 +7,15 @@ export const formSchema = z.object({
 })
 
 export const amountOptions = [
-  { value: "1", label: "1 photo" },
-  { value: "2", label: "2 photos" },
-  { value: "3", label: "3 photos" },
-  { value: "4", label: "4 photos" },
-  { value: "5", label: "5 photos" },
+  { value: "1", label: "1장" },
+  { value: "2", label: "2장" },
+  { value: "3", label: "3장" },
+  { value: "4", label: "4장" },
+  { value: "5", label: "5장" },
+]
+
+export const resolutionOptions = [
+  { value: "256x256", label: "256x256" },
+  { value: "512x512", label: "512x512" },
+  { value: "1024x1024", label: "1024x1024" },
 ]
