d33f0889fe6f8cc15b496e4131ecbd0b6c4bf4e6
mackh <kopserf@gmail.com>
2024-05-11 17:51:20 +0900

    🚀 ➖

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -10,7 +10,7 @@ const LandingPage = () => {
     <div className="h-full">
       <LandingNavbar/>
       <LandingAi/>
-      <LandingContent/>
+      {/* <LandingContent/> */}
       <LandingCowork/>
     </div>
     );
